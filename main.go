9a807d7ff04e635acdd313900460005b78772004
CaseyKelly <casey.kelly@colorado.edu>
2016-09-20 19:28:36 -0400

    add comments for program so far

--- main.go
+++ main.go
@@ -8,18 +8,22 @@ import (
 )
 
 func main() {
+	// Define command line flag for -url, save and parse input from user
 	url := flag.String("url", "", "url of instagram photo")
 	flag.Parse()
 
+	// If command line flag for -url is missing, ask user to provide it
 	if *url == "" {
 		fmt.Println("Use the -url flag and provide an instagram url")
 	} else {
+		// Otherwise get the html from the url
 		resp, err := http.Get(*url)
 		if err != nil {
 			panic(err)
 		}
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
+
 		fmt.Println("get:\n", string(body))
 	}
 }
