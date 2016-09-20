2b12b4b6bf06c640de9017149659cf1dbd3c1f27
CaseyKelly <casey.kelly@colorado.edu>
2016-09-20 19:11:28 -0400

    add url flag

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	url := flag.String("url", "", "url of instagram photo")
+	flag.Parse()
+
+	fmt.Println("url: ", *url)
 }
